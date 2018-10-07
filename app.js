b212ba671564595106fd6088c7444f337a53d009
sanidjaya <sanidjaya@gmail.com>
2018-10-07 10:41:42 -0400

    change goal amount bug fixes

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ app.post("/", function (request, response) {
   //Handler function for Nessie
   function handleCheckBalance(assistant) { 
     return handleCheckBalanceHttp().then(results => {
-        console.log(results);
         assistant.add(results);
     })
   }
@@ -70,7 +69,7 @@ app.post("/", function (request, response) {
     });
   }
 
-    // **************************
+  // **************************
   //  Add value to child account
   // **************************
 
@@ -87,8 +86,6 @@ app.post("/", function (request, response) {
 
   function handleAddValueToChildHttp(transferAmount) {
     const addValueUrl = NESSIE_API_URL + PARENT_CUSTOMER_ACCOUNT +"/transfers?key="+ NESSIE_API_KEY;
-    console.log(addValueUrl)
-    console.log(transferAmount)    
 
     return httpRequest({
         method: "POST",
@@ -111,7 +108,7 @@ app.post("/", function (request, response) {
     });
   }
 
-    //******************************
+  //******************************
   // Changing Target Goal 
   //******************************
 
@@ -121,7 +118,7 @@ app.post("/", function (request, response) {
   function handleChangeGoal(assistant){
   	//sets the new goal amount to goalAmount
   	const GOAL_AMOUNT_ARG = "goalAmount"
-  	const goalAmount = parseFloat(assistant.getArgument(GOAL_AMOUNT_ARG));
+    const goalAmount = parseFloat(assistant.parameters.goalAmount.amount);
 
   	return handleChangeGoalHttp(goalAmount).then(results => {
   		console.log(results);
@@ -130,164 +127,56 @@ app.post("/", function (request, response) {
   }
 
   function handleChangeGoalHttp(goalAmount){
-  	const childGoalAPIUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT +"?key="+ NESSIE_API_KEY;
+      const childGoalAPIUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT +"?key="+ NESSIE_API_KEY;
 
-  	var oldGoal = httpRequest({
+  	return httpRequest({
   		method: "GET",
   		uri: childGoalAPIUrl,
   		json: true
   		}).then(function (json) {
-  		const oldGoalBalance = utilities.findBalance(json);
-  		return oldGoalBalance; 
+          var oldGoal = json.balance;
+          return oldGoal;
   		}).catch(function (err) {
         console.log("Error:" + err);
-    });
-  		
-  	const childGoalDepositUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT + "/deposits?key=" + NESSIE_API_KEY; 
-  	const childGoalWithdrawalUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT + "/withdrawals?key=" + NESSIE_API_KEY
-  	if(goalAmount > oldGoal){
-  		return httpRequest({
-  			method: "POST",
-      		uri: childGoalDepositUrl,
-      		json: true,
-      		body:{
-      			"medium" : "balance",
-      			"transaction_date": "2018-10-06",
-      			"status": "pending",
-      			"description": "string"
-      			"amount": goalAmount - oldGoal
-          }}).then(function(json){
-          	const speech = utilities.changeGoal(json, goalAmount);
-          	return speech;
-          }).catch(function(err){
-          	console.log("Error:" + err);
-          });
-      }
-      else{
-      	return httpRequest({
-      		method: "POST", 
-      		uri: childGoalWithdrawalUrl,
-      		json: true,
-      		body:{
-      			"medium": "balance",
-      			"transaction_date": "2018-10-07",
-      			"status": "pending",
-      			"amount": oldGoal - goalAmount,
-      			"description": "string"
-      		}}).then(function(json){
-      			const speech = utilities.changeGoal(json, goalAmount);
-      			return speech; 
-      		}).catch(function(err){
-      			console.log("Error:" + err);
-      		});
-      }
-  }
-
-  //*****************************
-  // Transfer Action
-  //*****************************
-
-  //Action name for getting last transaction
-  const TRANSFER_MONEY_ACTION = "transferMoney"
-  //Handler function for getting the last transaction
-  function handleTransferMoney(assistant) {
-    //1. Declare amount to be transferred (input from user)
-    const TRANSFER_AMOUNT_ARG = "transferAmount"
-    //2. Extract day of week from the assistant
-    const transferAmount = parseFloat(assistant.getArgument(TRANSFER_AMOUNT_ARG));
-    const nessieAPIUrl = NESSIE_API_URL + CUSTOMER_ACCOUNT +"/transfers?key="+ NESSIE_API_KEY;
-
-    httpRequest({
-      method: "POST",
-      uri: nessieAPIUrl,
-      json: true,
-      body: {
-          "medium": "balance",
-          "payee_id": "59275453ceb8abe24250de9c",
-          "amount": transferAmount,
-          "transaction_date": "2017-05-25",
-          "description": "string"
-      }
-    }).then(function(json){
-      const speech = utilities.transferMoney(json);
-      utilities.replyToUser(request, response,assistant, speech);
     })
-    .catch(function(err){
-      console.log("Error:" + err);
-      const speech = "I cannot understand that request. Ask me something else";
-      utilities.replyToUser(request, response, assistant, speech);
-    });
-  }
-
-  //*****************************
-  // Find Bill Action
-  //*****************************
-
-  //Action name for getting last transaction
-  const FIND_BILL = "findBill"
-  //Handler function for getting the last transaction
-  function handleFindBill(assistant) {
-    const nessieAPIUrl = NESSIE_API_URL + CUSTOMER_ACCOUNT +"/bills?key="+ NESSIE_API_KEY;
-    httpRequest({
-      method: "GET",
-      uri: nessieAPIUrl,
-      json: true
-    }).then(function(json){
-      const speech = utilities.findBill(json);
-      utilities.replyToUser(request, response,assistant, speech);
+    .then(function(oldGoal) {
+        const childGoalDepositUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT + "/deposits?key=" + NESSIE_API_KEY; 
+        const childGoalWithdrawalUrl = NESSIE_API_URL + CHILD_GOAL_ACCOUNT + "/withdrawals?key=" + NESSIE_API_KEY
+        console.log("Goal amount: " + goalAmount)
+        console.log("Old Amount: "+ oldGoal)
+          if(goalAmount > oldGoal){
+              return httpRequest({
+                  method: "POST",
+                  uri: childGoalDepositUrl,
+                  json: true,
+                  body:{
+                      "medium" : "balance",
+                      "description": "string",
+                      "amount": goalAmount - oldGoal
+              }}).then(function(json){
+                  const speech = utilities.changeGoal(json, goalAmount);
+                  return speech;
+              }).catch(function(err){
+                  console.log("Error:" + err);
+              });
+          }
+          else{
+              return httpRequest({
+                  method: "POST", 
+                  uri: childGoalWithdrawalUrl,
+                  json: true,
+                  body:{
+                      "medium": "balance",
+                      "amount": oldGoal - goalAmount,
+                      "description": "string"
+                  }}).then(function(json){
+                      const speech = utilities.changeGoal(json, goalAmount);
+                      return speech; 
+                  }).catch(function(err){
+                      console.log("Error:" + err);
+                  });
+          }
     })
-    .catch(function(err){
-      console.log("Eror:"+err);
-      const speech = "I cannot understand that request. Ask me something else";
-      utilities.replyToUser(request, response, assistant, speech);
-    });
-  }
-
-  //*****************************
-  // Pay Bill Action
-  //*****************************
-
-  const PAY_BILL = "payBill"
-  //Handler function for getting the last transaction
-  function handlePayBill(assistant) {
-    const nessieAPIUrl = NESSIE_API_URL + CUSTOMER_ACCOUNT +"/bills?key="+ NESSIE_API_KEY;
-    httpRequest({
-      method: "GET",
-      uri: nessieAPIUrl,
-      json: true
-    }).then(function(json){
-      const speech = utilities.payBill(json);
-      utilities.replyToUser(request, response,assistant, speech);
-    })
-    .catch(function(err){
-      console.log("Eror:"+err);
-      const speech = "I cannot understand that request. Ask me something else";
-      utilities.replyToUser(request, response, assistant, speech);
-    });
-  }
-
-  //*****************************
-  // Save Money Action
-  //*****************************
-  //Action name for getting last transaction
-  const SAVE_MONEY_ACTION = "saveMoney"
-  //Handler function for getting the last transaction
-  function handleSaveMoney(assistant) {
-    const nessieAPIUrl = NESSIE_API_URL + CUSTOMER_ACCOUNT + "/purchases?key=" + NESSIE_API_KEY;
-
-    httpRequest({
-      method: "GET",
-      uri: nessieAPIUrl,
-      json: true,
-    }).then(function(json){
-      const speech = utilities.saveMoney(json);
-      utilities.replyToUser(request, response, assistant, speech);
-    })
-    .catch(function(err){
-      console.log("Error:" + err);
-      const speech = "I cannot understand that request. Ask me something else";
-      utilities.replyToUser(request, response, assistant, speech);
-    });
   }
   //Create a map of potential actions that a user can trigger
   const actionMap = new Map();
@@ -297,12 +186,6 @@ app.post("/", function (request, response) {
   actionMap.set(CHECK_BALANCE_ACTION, handleCheckBalance);
   actionMap.set(ADD_VALUE_ACTION, handleAddValueToChild);
   actionMap.set(CHANGE_GOAL_ACTION, handleChangeGoal);
-  actionMap.set(TRANSFER_MONEY_ACTION, handleTransferMoney);
-  actionMap.set(FIND_BILL, handleFindBill);
-  actionMap.set(PAY_BILL, handlePayBill);
-  actionMap.set(SAVE_MONEY_ACTION, handleSaveMoney);
-
-  console.log(actionMap);
 
   //Register the action map with the assistant
   assistant.handleRequest(actionMap);
--- utilities.js
+++ utilities.js
@@ -1,6 +1,9 @@
 const SerialPort = require('serialport')
 const port = new SerialPort('/dev/tty.usbmodem14131')
 
+// A = LED AND EARS
+// B = EARS
+// C = LEDS
 
 const financeTips = require("./financeTips.json");
 const tickersData = require("./companyTickers.json");
@@ -9,6 +12,7 @@ module.exports = {
     findBalance: function(json) {
         const balanceAmount = json.balance
         var speech = ("Your balance is $" + balanceAmount)
+
 
         port.write('B', function(err) {
             if (err) {
@@ -20,12 +24,11 @@ module.exports = {
         console.log("findBalance hit")
         return speech;
     },
-<<<<<<< Updated upstream
     addValueToChildAccount: function(json) {
         const valueAdded = json.objectCreated.amount
         var speech = ("Sure thing! I added " + valueAdded + " into my tummy.")
 
-        //sends alfeka commands
+        // sends alfeka commands
         port.write('A', function(err) {
             if (err) {
               return console.log('Error on write: ', err.message)
@@ -36,77 +39,17 @@ module.exports = {
         console.log("addValue hit")
         return speech;
     },
-    changeGoal: function(json) {
-        const goalAmount = json.balance
-=======
     changeGoal: function(json, goalAmount) {
->>>>>>> Stashed changes
         var speech = ("Your new goal is $" + goalAmount)
+
+        port.write('C', function(err) {
+            if (err) {
+              return console.log('Error on write: ', err.message)
+            }
+            console.log('A message written')
+        })
 
         console.log("changeGoal hit")
         return speech; 
-    },
-
-    transferMoney: function(json) {
-        const amountTransferred = json.objectCreated.amount
-        var speech = ("You have transferred $" + amountTransferred + " to Mark's account")
-        
-        console.log("transferMoney hit")
-        say.speak(speech)
-        return speech
-    },
-    saveMoney: function(json) {
-        const transactionAmount = 0;
-        var hash = {}
-        var category
-
-        amountsArr = json.map(function(o) {
-            return o.amount;
-        })
-        categoriesArr = json.map(function(o) {
-            return o.description;
-        })
-
-        for (category in categoriesArr) {
-            hash[categoriesArr[category]] = amountsArr[category]
-        }
-
-        var maxCategory = Object.keys(hash).reduce(function(a, b) {
-            return hash[a] > hash[b] ? a : b
-        })
-
-        var maxCategoryAmount = hash.health.toString()
-
-        var speech = ("In the last month, you've spent a grand total of $"
-        + maxCategoryAmount + " on " + maxCategory + ". This was your highest "
-        + "expense. " + financeTips[maxCategory])
-
-        console.log("saveMoney hit")
-        say.speak(speech)
-        return speech
-    },
-    findBill: function(json){
-        const status = json[0].status
-        const payee = json[0].payee
-        const payment_date = json[0].payment_date
-        const payment_amount = json[0].payment_amount
-        const recurring_date= json[0].recurring_date
-        var speech = ("You have one bill due. Your " + payee + " bill is due on "+ payment_date+" in the amount of $"+ payment_amount+". This bill reoccurs on the "+ recurring_date +"st of every month.")
-       
-        console.log("findBill hit")
-        say.speak(speech)
-        return speech;
-    },
-    payBill: function(json){
-        const status = json[0].status
-        const payee = json[0].payee
-        const payment_date = json[0].payment_date
-        const payment_amount = json[0].payment_amount
-        const recurring_date= json[0].recurring_date
-        var speech = "$" + payment_amount + ".00 has been applied to your Capital One Credit Card bill. Your amount due is now $0.00.";
-       
-        console.log("findBill hit")
-        say.speak(speech)
-        return speech;
     }
 }
